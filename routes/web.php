fa7f91a5b0768de45b0b47ed75664ccf90270dc1
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-02-13 20:18:32 +0100

    cleaning up

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    
 });
+
+
+
